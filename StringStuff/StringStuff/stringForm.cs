f73cb10cb09d72097561c9b69706ca8e24ce961d
TylerRitt <35239059+TylerRitt@users.noreply.github.com>
2018-02-12 10:31:45 -0800

    final lab 2

---
+++ StringStuff/StringStuff/stringForm.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace String_Stuff
+{
+    public partial class stringForm : Form
+    {
+        public stringForm()
+        {
+            InitializeComponent();
+        }
+
+        string code = "qwertyuiopasdfghjklzxcvbnm";
+
+        private string SwitchCase(string input)
+        {
+
+           
+            string output = "";
+          
+            foreach (char c in input)
+            {
+
+               if (Char.IsUpper(c))
+                {
+                 output += Char.ToLower(c);
+                }
+                
+               else if (Char.IsLower(c))
+                {
+                   output+= Char.ToUpper(c);
+                }
+               else if (c == ' ')
+                {
+                    output += c;
+                }
+              
+            }
+
+           
+            switchCaseTextBox.Text = output;
+
+
+
+            return output;
+        }
+
+        private string Reverse(string input)
+        {
+
+            string output = "";
+
+            for ( int i = input.Length -1; i >= 0; i--)
+            {
+                output += input[i];
+
+            }
+
+
+
+
+
+
+            return output;
+        }
+
+        private string PigLatin(string input)
+        {
+            string output = "";
+            string[] words;
+            words = input.Split(' ');
+            string temp;
+            foreach (string word in words)
+            {
+                
+                temp = word;
+                temp += temp[0];
+               temp = temp.Remove(0,1);
+                temp += "ay ";
+                output += temp.ToLower();
+            }
+            
+            
+            return output; 
+    }
+
+        private string ShiftCypher(string input, int charsToShift)
+        {
+
+            string output = "";
+            char temp;
+
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            foreach(char c in input)
+            {
+                if (c == ' ')
+                {
+                    output += ' ';
+                }
+                else
+                {
+                    temp = Char.ToLower(c); // had to shift to lowercase
+                    int index = alpha.IndexOf(temp);
+                    index += charsToShift;
+                    if (index >= 26)
+                        index -= 26;
+                    output += alpha[index];
+                }
+            }
+
+                
+                // index 28 -26 = 2 
+           
+            
+            return output;
+        }
+
+        private string SubCypher(string input, string code)
+        {
+            // maybe put as global? code = "qwertyuiopasdfghjklzxcvbnm";
+            
+            string output = "";
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            char temp;
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    output += ' ';
+                }
+                else
+                {
+                    temp = Char.ToLower(c); // had to shift to lowercase
+                    int index = alpha.IndexOf(temp);
+                    output += code[index];
+                }
+            }
+
+            return output;
+        }
+
+        private void transformButton_Click(object sender, EventArgs e)
+        {
+
+            string input = inputTextBox.Text;
+            switchCaseTextBox.Text = SwitchCase(input);
+            subTextBox.Text = SubCypher(input, code);
+            pigLatinTextBox.Text = PigLatin(input);
+            shiftTextBox.Text = ShiftCypher(input, 4);
+            reverseTextBox.Text = Reverse(input);
+
+        }
+    }
+}
---
+++ Tutorial 8-4/CSV Reader/CSV Reader/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double Average(int[] scores)
+        {
+            double average = 0;
+            int total = 0;
+
+            // Calculate the total of the
+            // test score tokens.
+
+            // Calculate the average of these
+            // test scores.
+            return average;
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader inputFile;  // To read the file
+                string line;   // To hold a line from the file
+                int count = 0;
+                int total;
+                double average;          // Test score average
+
+                // Create a delimiter array.
+                char[] delim = { ',' };
+
+                // Open the CSV file.
+                inputFile = File.OpenText("..\\..\\Grades.csv");
+
+                while (!inputFile.EndOfStream)
+                {
+                    count++;
+                    line = inputFile.ReadLine();
+                    string[] tokens = line.Split(delim);
+                    total = 0;
+
+                    foreach(string str in tokens)
+                    {
+                        total += int.Parse(str);
+                    }
+                    average = (double)total / tokens.Length;
+
+                    averagesListBox.Items.Add("The average for student " +
+                        count + " is " + average.ToString("n1"));
+                }
+
+                // Close the file.
+                inputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                // Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
